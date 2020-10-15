9e0dbaedeb90fc794b181866aa57e610b1b62724
lichengru <gjo4us06c961115@gmail.com>
2020-10-15 20:42:09 +0800

    加入專案檔案。

---
+++ Form1.Designer.cs
@@ -0,0 +1,70 @@
+﻿namespace W202010151
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(150, 255);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(279, 100);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(150, 111);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(246, 22);
+            this.textBox1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(572, 502);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace W202010151
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+       
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            {
+
+                int randomNum = new Random().Next(100);
+                try
+                {
+                    string aa = textBox1.Text;
+                    int input_int = int.Parse(aa);
+
+                    if (randomNum == input_int)
+                    {
+                        MessageBox.Show("ok", "骰子", MessageBoxButtons.OK);
+                    }
+                    else
+                    {
+                        MessageBox.Show("88", "骰子", MessageBoxButtons.OK);
+                    }
+
+
+
+
+                }
+                catch
+                {
+                    MessageBox.Show("別鬧", "骰子", MessageBoxButtons.OK);
+                }
+
+
+            }
+        }
+    }
+}
